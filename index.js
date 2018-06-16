fda5140906a1063fc89333345c16ea6dd3697034
Theodore Byron <ted@tedbyron.com>
2018-06-15 21:08:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,40 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron');
+// modules to control application life and create native browser window
+const {app, BrowserWindow} = require('electron')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+// global reference of the window object
+let mainWindow
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1200, height: 900});
+// method call after electron initialization
+app.on('ready', createWindow)
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html');
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-
-// Quit when all windows are closed.
+// quit when all windows are closed unless platform is OS X
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
+})
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
+  // in OS X, re-create a window when dock icon is clicked and there are no other windows open
   if (mainWindow === null) {
-    createWindow();
+    createWindow()
   }
-});
+})
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+function createWindow () {
+  // create browser window
+  mainWindow = new BrowserWindow({width: 1200, height: 800})
+
+  // load index.html
+  mainWindow.loadFile('index.html')
+
+  // open devtools
+  // mainWindow.webContents.openDevTools()
+
+  // emitted when the window is closed
+  mainWindow.on('closed', function () {
+    // dereference the window object, store windows in an array if
+    // there are multiple windows
+    mainWindow = null
+  })
+}
